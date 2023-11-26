27aa522346230e4574dafe1397d50bc4651ea438
shibuya-yuta-sy <shibu31356@yahoo.ne.jp>
2023-11-26 18:52:05 +0900

    github公開設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 
+const base = "/portfolio-site";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://yuta-shibuya-sy.github.io",
+  base: base,
   integrations: [svelte()],
 });
---
+++ src/lib/utils/path.ts
@@ -0,0 +1,14 @@
+const base = "/portfolio-site";
+
+export const path = (path: string) => {
+  // pathが/から始まっていなかったら追加する
+  if (!path.startsWith("/")) {
+    path = `/${path}`;
+  }
+
+  // @ts-ignore
+  if (base === "/") {
+    return path;
+  }
+  return `${base}${path}`;
+};
