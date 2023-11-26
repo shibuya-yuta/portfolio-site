8ab39a0079036038471b745f2a8a147e82ee31b3
shibuya-yuta-sy <shibu31356@yahoo.ne.jp>
2023-11-26 19:05:46 +0900

    公開作業

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 
-const base = "/portfolio-site";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://yuta-shibuya-sy.github.io",
-  base: base,
+  base: "/portfolio-site",
   integrations: [svelte()],
 });
